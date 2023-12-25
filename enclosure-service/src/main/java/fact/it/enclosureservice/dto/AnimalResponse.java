82d475d1427101e11103b2870bb16eb6448fe9af
NatanVBraeckel <r0804023@student.thomasmore.be>
2023-12-25 20:37:40 +0100

    Enclosure unit tests

--- enclosure-service/src/main/java/fact/it/enclosureservice/dto/AnimalResponse.java
+++ enclosure-service/src/main/java/fact/it/enclosureservice/dto/AnimalResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -17,6 +18,6 @@ public class AnimalResponse {
     private String animalCode;
     private String name;
     private String type;
-    private Date birthDate;
+    private LocalDate birthDate;
     private FoodResponse preferredFood;
 }
